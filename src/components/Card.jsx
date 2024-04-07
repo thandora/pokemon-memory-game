614a420dd779c09dd55c36e738e8644a0e25250c
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-04-07 21:03:59 +0800

    life score and clickedId states up to App

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,24 @@
 import PropTypes from "prop-types";
-import "../styles/Card.css"
+import "../styles/Card.css";
 
-function Card({ name, imgUrl }) {
+function Card({ pokeId, name, imgUrl, scoreState, clickedIdsState }) {
+  const [score, setScore] = scoreState;
+  const [clickedIds, setClickedIds] = clickedIdsState;
+
+  function scoreHandler(id) {
+    return () => {
+      if (!clickedIds.includes(id)) {
+        setClickedIds([...clickedIds, id]);
+        setScore(score + 1);
+      } else {
+        setClickedIds([]);
+        setScore(0);
+      }
+    };
+  }
+
   return (
-    <div className="pokemon-card">
+    <div className="pokemon-card" onClick={scoreHandler(pokeId)}>
       <img src={imgUrl} alt={name} />
       <h2>{name}</h2>
     </div>
@@ -11,8 +26,12 @@ function Card({ name, imgUrl }) {
 }
 
 Card.propTypes = {
+  pokemonList: PropTypes.array,
+  pokeId: PropTypes.number,
   name: PropTypes.string,
   imgUrl: PropTypes.string,
+  scoreState: PropTypes.array,
+  clickedIdsState: PropTypes.array,
 };
 
 export { Card };
