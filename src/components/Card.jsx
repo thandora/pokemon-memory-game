87185147a9c56cfb2568af30efd1a5e327fab43c
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-04-07 12:35:24 +0800

    create Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,17 @@
 import PropTypes from "prop-types";
 
-function Card({ name, imgUrl }) {}
+function Card({ name, imgUrl }) {
+  return (
+    <div>
+      <img src={imgUrl} alt={name} />
+      <h2>{name}</h2>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  name: PropTypes.number,
+  imgUrl: PropTypes.string,
+};
+
+export { Card };
