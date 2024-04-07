3a9b06833e9a94c7d261835229b4a92559d804ea
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-04-07 21:07:02 +0800

    create testData
    testData contains data for quick testing, reducing API calls

---
+++ src/testData.js
@@ -0,0 +1,39 @@
+const pokemonList = [
+  {
+    id: 929,
+    name: "dolliv",
+    spriteUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/929.png",
+    types: ["grass", "normal"],
+  },
+  {
+    id: 191,
+    name: "sunkern",
+    spriteUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/191.png",
+    types: ["grass"],
+  },
+  {
+    id: 750,
+    name: "mudsdale",
+    spriteUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/750.png",
+    types: ["ground"],
+  },
+  {
+    id: 69,
+    name: "bellsprout",
+    spriteUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/69.png",
+    types: ["grass", "poison"],
+  },
+  {
+    id: 203,
+    name: "girafarig",
+    spriteUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/203.png",
+    types: ["normal", "psychic"],
+  },
+];
+
+export { pokemonList };
